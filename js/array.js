c77531b37b51507c1ca7d27239e633b7c1be0a47
Dmitry Korobkin <dmitry.korobkin@gmail.com>
2012-02-23 05:46:50 +0400

    1. Исправлен $Array.toArray 2. Array.prototype.slice.call(NodeList) вызывает ошибку в IE<9, поэтому в fix.js метод $Array.toArray перезаписан с использованием цикла 3. $(array).count() теперь возвращает реальное количество элементов в массиве (http://javascript.ru/forum/misc/25392-rabota-s-massivom.html#post155335)

--- js/array.js
+++ js/array.js
@@ -10,10 +10,21 @@ function $Array(array) {
 
 $.Array = $Object.extend($Array, {
 
+	/**
+	 * @see Array.isArray
+	 */
 	isArray: Array.isArray,
 
-	toArray: function () {
-		return Array.prototype.slice.apply(list, arguments);
+	/**
+	 * Преобразует объект в массив
+	 * @param {Mixed} anything объект с числовыми ключами и свойством length
+	 * @param {Number} [begin]
+	 * @param {Number} [end]
+	 * @return {Array}
+	 */
+	toArray: function (anything, begin, end) {
+		//этот метод перезаписывается в fix.js для IE<9
+		return Array.prototype.slice.call(anything, begin, end);
 	}
 
 });
@@ -23,7 +34,11 @@ $Array.prototype = $Object.extend(Object.create($.prototype), {
 	constructor: $Array,
 
 	count: function () {
-		return this.src.length;
+		//подсчет реального количсества элементов
+		//http://javascript.ru/forum/misc/25392-rabota-s-massivom.html#post155335
+		return this.src.reduce(function (length) {
+			return length + 1;
+		}, 0);
 	},
 
 	nth: function () {
--- js/test.js
+++ js/test.js
@@ -1,7 +1,1 @@
-﻿/*
-Array.prototype.count = function () {
-	return this.reduce(function (x) {
-		return x + 1;
-	}, 0);
-};
-*/
+﻿
