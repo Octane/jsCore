2996efb013008c246724e7beeade4ecec36094de
Dmitry Korobkin <dmitry.korobkin@gmail.com>
2014-03-31 06:05:35 +0400

    DOM4 methods, fix element traversal for document
    New methods: append, prepend, before, after, replace, remove, query,
    queryAll, matches

--- jscore.js
+++ jscore.js
@@ -891,11 +891,9 @@ function StaticDOMStringMap() {}
 			return true;
 		},
 
-		contains: function () {
+		contains: function (token) {
 			this.update();
-			return !Array.find(arguments, function (token) {
-				return Array.indexOf(this, token) == -1;
-			}, this);
+			return Array.indexOf(this, token) != -1;
 		},
 
 		toString: function () {
@@ -944,44 +942,10 @@ function StaticDOMStringMap() {}
 
 };
 
-HTMLElement.prototype.matches || new function () {
-
-	var proto = HTMLElement.prototype;
-
-	function isContains(root, element, selector) {
-		return Array.indexOf(root.querySelectorAll(selector), element) != -1;
-	}
-
-	function matches(selector) {
-		var root = this.parentNode, contains;
-		if (root) {
-			if (root.nodeType == Node.ELEMENT_NODE) {
-				root = root.ownerDocument;
-			}
-			return isContains(root, this, selector);
-		}
-		root = document.createDocumentFragment();
-		root.appendChild(this);
-		contains = isContains(root, this, selector);
-		root.removeChild(this);
-		return contains;
-	}
-
-	proto.matches = [
-		proto.matchesSelector,
-		proto.oMatchesSelector,
-		proto.msMatchesSelector,
-		proto.mozMatchesSelector,
-		proto.webkitMatchesSelector,
-		matches
-	].find(Boolean);
-
-};
-
 //Element traversal polyfill
 "children" in document.createDocumentFragment() || new function () {
 
-	var api = {
+	var proto, api = {
 
 		firstElementChild: function () {
 			var node = this.firstChild;
@@ -1047,16 +1011,144 @@ HTMLElement.prototype.matches || new function () {
 
 	};
 
-	[document.createDocumentFragment().constructor, HTMLElement].forEach(function (Constructor) {
-		var proto = Constructor.prototype;
-		Object.keys(api).forEach(function (key) {
-			if (!(key in proto)) {
-				Object.defineProperty(proto, key, {
-					get: api[key]
-				});
-			}
-		});
+	function defineGetter(key) {
+		if (!(key in proto)) {
+			Object.defineProperty(proto, key, {
+				get: api[key]
+			});
+		}
+	}
+
+	proto = HTMLElement.prototype;
+	Object.keys(api).forEach(defineGetter);
+
+	[
+		document.constructor,
+		document.createDocumentFragment().constructor
+	].forEach(function (Constructor) {
+		proto = Constructor.prototype;
+		[
+			"firstElementChild", "lastElementChild",
+			"childElementCount", "children"
+		].forEach(defineGetter);
 	});
+
+};
+
+//DOM4 http://www.w3.org/TR/dom/#element
+"append" in document.createDocumentFragment() || new function () {
+
+	//todo HierarchyRequestError
+
+	var api, proto = HTMLElement.prototype;
+
+	function isContains(root, element, selector) {
+		return Array.indexOf(root.querySelectorAll(selector), element) != -1;
+	}
+
+	function mutationMacro(nodes) {
+		var length = nodes.length, i, node, fragment;
+		if (length == 1) {
+			node = nodes[0];
+			if (typeof node == "string") {
+				return document.createTextNode(node);
+			}
+			return node;
+		}
+		fragment = document.createDocumentFragment();
+		nodes = Array.from(nodes);
+		i = 0;
+		while (i < length) {
+			node = nodes[i];
+			if (typeof node == "string") {
+				node = document.createTextNode(node);
+			}
+			fragment.appendChild(node);
+			i++;
+		}
+		return fragment;
+	}
+
+	api = {
+
+		before: function (/* ...nodes */) {
+			//todo IE8 removedNode.parentNode != null
+			this.parentNode.insertBefore(mutationMacro(arguments), this);
+		},
+
+		after: function (/* ...nodes */) {
+			var parentNode = this.parentNode,
+				nextSibling = this.nextSibling,
+				nodes = mutationMacro(arguments);
+			if (nextSibling) {
+				parentNode.insertBefore(nodes, nextSibling);
+			}
+			else {
+				parentNode.appendChild(nodes);
+			}
+		},
+
+		replace: function (/* ...nodes */) {
+			this.parentNode.replaceChild(mutationMacro(arguments), this);
+		},
+
+		remove: function () {
+			this.parentNode.removeChild(this);
+		},
+
+		append: function (/* ...nodes */) {
+			this.appendChild(mutationMacro(arguments));
+		},
+
+		prepend: function () {
+			this.insertBefore(mutationMacro(arguments), this.firstChild);
+		},
+
+		query: function (selector) {
+			return this.querySelector(selector);
+		},
+
+		queryAll: function (selector) {
+			return this.querySelectorAll(selector);
+		},
+
+		matches: [
+			proto.matchesSelector,
+			proto.oMatchesSelector,
+			proto.msMatchesSelector,
+			proto.mozMatchesSelector,
+			proto.webkitMatchesSelector,
+			function (selector) {
+				var root = this.parentNode, contains;
+				if (root) {
+					if (root.nodeType == Node.ELEMENT_NODE) {
+						root = root.ownerDocument;
+					}
+					return isContains(root, this, selector);
+				}
+				root = document.createDocumentFragment();
+				root.appendChild(this);
+				contains = isContains(root, this, selector);
+				root.removeChild(this);
+				return contains;
+			}
+		].find(Boolean)
+
+	};
+
+	function implement(key) {
+		if (!(key in proto)) {
+			proto[key] = api[key];
+		}
+	}
+
+	Object.keys(api).forEach(implement);
+
+	proto = document.constructor;
+	["query", "queryAll"].forEach(implement);
+
+	proto = document.createDocumentFragment().constructor.prototype;
+	["append", "prepend", "query", "queryAll", "matches"].forEach(implement);
 
 };
 
