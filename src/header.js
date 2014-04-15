53717585fa86599324366f015c2cb54f784cc27f
Dmitry Korobkin <dmitry.korobkin@gmail.com>
2014-04-15 22:27:31 +0400

    copyright

--- src/header.js
+++ src/header.js
@@ -1,8 +1,7 @@
 "use strict";
 
-/**
- * jsCore JavaScript library v0.1
- * Copyright 2014, Dmitry Korobkin
+/* jsCore JavaScript library v0.1
+ * © 2014 Dmitry Korobkin
  * Released under the MIT license
  * https://github.com/Octane/jsCore/
  */
