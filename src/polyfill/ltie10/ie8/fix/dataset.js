cd874b672a1db95e58ca90a1b56a352342d0d29b
Dmitry Korobkin <dmitry.korobkin@gmail.com>
2014-05-03 22:43:10 +0400

    avoid global closure

--- src/polyfill/ltie10/ie8/fix/dataset.js
+++ src/polyfill/ltie10/ie8/fix/dataset.js
@@ -1,21 +1,23 @@
 
 //IE8 dataset polyfill fix
-try {
-    Object.defineProperty({}, 'test', {});
-}
-catch (error) {
-    window.StaticDOMStringMap = new function () {
-        //https://github.com/es-shims/es5-shim/issues/152
-        var uid = 0,
-            fakeDoc = new ActiveXObject('htmlfile'),
-            proto = createObject().constructor.prototype;
-        function createObject() {
-            return fakeDoc.getElementsByName(uid++);
-        }
-        Object.keys(proto).forEach(function (key) {
-            proto[key] = undefined;
-        });
-        proto = null;
-        return createObject;
-    };
-}
+new function () {
+    try {
+        Object.defineProperty({}, 'test', {});
+    }
+    catch (error) {
+        window.StaticDOMStringMap = new function () {
+            //https://github.com/es-shims/es5-shim/issues/152
+            var uid = 0,
+                fakeDoc = new ActiveXObject('htmlfile'),
+                proto = createObject().constructor.prototype;
+            function createObject() {
+                return fakeDoc.getElementsByName(uid++);
+            }
+            Object.keys(proto).forEach(function (key) {
+                proto[key] = undefined;
+            });
+            proto = null;
+            return createObject;
+        };
+    }
+};
